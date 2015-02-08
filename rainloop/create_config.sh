821ff84fa21200e3571717d22fbecab6d3798a36
domse <domse@users.noreply.github.com>
2015-02-08 15:49:24 +0100

    Changed create_config
    Corrected misspelling

--- rainloop/create_config.sh
+++ rainloop/create_config.sh
@@ -4,8 +4,8 @@ for i in $(cat /domains);
 imap_host = \"$i\" 
 imap_port = 143
 imap_secure = \"TLS\"
-smpt_host = \"$i\"
-smpt_port = 587
-smpt_secure = \"TLS\"
-smpt_auth = On" > $DIR/_default_/domains/$i.ini;
+smtp_host = \"$i\"
+smtp_port = 587
+smtp_secure = \"TLS\"
+smtp_auth = On" > $DIR/_default_/domains/$i.ini;
 done;
